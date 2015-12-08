bf2860cc200e5e556bacce8f29a6a22b116d3dae
Christopher deCharms <cdecharms@MacBook-Pro-3.local>
2015-12-07 21:03:24 -0800

    Removed code from plugin related to audio to get it to compile

--- src/ios/PCObserver.swift
+++ src/ios/PCObserver.swift
@@ -2,25 +2,25 @@ import Foundation
 
 class PCObserver : NSObject, RTCPeerConnectionDelegate {
     var session: Session
-
+    
     init(session: Session) {
         self.session = session
     }
-
+    
     func peerConnection(peerConnection: RTCPeerConnection!,
         addedStream stream: RTCMediaStream!) {
         print("PCO onAddStream.")
-
+            
         dispatch_async(dispatch_get_main_queue()) {
             if stream.videoTracks.count > 0 {
                 self.session.addVideoTrack(stream.videoTracks[0] as! RTCVideoTrack)
             }
         }
-
+        
         self.session.sendMessage(
             "{\"type\": \"__answered\"}".dataUsingEncoding(NSUTF8StringEncoding)!)
     }
-
+    
     func peerConnection(peerConnection: RTCPeerConnection!,
         removedStream stream: RTCMediaStream!) {
         print("PCO onRemoveStream.")
@@ -31,28 +31,22 @@ class PCObserver : NSObject, RTCPeerConnectionDelegate {
             }
         }*/
     }
-
+    
     func peerConnection(peerConnection: RTCPeerConnection!,
         iceGatheringChanged newState: RTCICEGatheringState) {
         print("PCO onIceGatheringChange. \(newState)")
-
+        
     }
-
+    
     func peerConnection(peerConnection: RTCPeerConnection!,
         iceConnectionChanged newState: RTCICEConnectionState) {
-          print("\(NSDate()): PCO onIceConnectionChange. \(newState) : \(newState.value)")
-          if(newState.value == 2) {
-              self.session.sendMessage("{\"type\": \"__connected\"}".dataUsingEncoding(NSUTF8StringEncoding)!);
-              dispatch_async(dispatch_get_main_queue()) {
-                self.session.plugin.onSessionConnected()
-              }
-          }
+        print("PCO onIceConnectionChange. \(newState)")
     }
-
+    
     func peerConnection(peerConnection: RTCPeerConnection!,
         gotICECandidate candidate: RTCICECandidate!) {
         print("PCO onICECandidate.\n  Mid[\(candidate.sdpMid)] Index[\(candidate.sdpMLineIndex)] Sdp[\(candidate.sdp)]")
-
+            
         var jsonError: NSError?
 
         let json: AnyObject = [
@@ -61,7 +55,7 @@ class PCObserver : NSObject, RTCPeerConnectionDelegate {
             "id": candidate.sdpMid,
             "candidate": candidate.sdp
         ]
-
+            
         let data: NSData?
         do {
             data = try NSJSONSerialization.dataWithJSONObject(json,
@@ -70,24 +64,24 @@ class PCObserver : NSObject, RTCPeerConnectionDelegate {
             jsonError = error
             data = nil
         }
-
+            
         self.session.sendMessage(data!)
     }
-
+    
     func peerConnection(peerConnection: RTCPeerConnection!,
         signalingStateChanged stateChanged: RTCSignalingState) {
         print("PCO onSignalingStateChange: \(stateChanged)")
     }
-
+    
     func peerConnection(peerConnection: RTCPeerConnection!,
         didOpenDataChannel dataChannel: RTCDataChannel!) {
         print("PCO didOpenDataChannel.")
     }
-
+    
     func peerConnectionOnError(peerConnection: RTCPeerConnection!) {
         print("PCO onError.")
     }
-
+    
     func peerConnectionOnRenegotiationNeeded(peerConnection: RTCPeerConnection!) {
         print("PCO onRenegotiationNeeded.")
         // TODO: Handle this
